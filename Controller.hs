f1cc0222da17f028303f19875172fc5c30eb15f6
joaozinhocc <joao.soares@ccc.ufcg.edu.br>
2019-11-25 21:46:41 -0300

    ajeitando a poha toda

--- Controller.hs
+++ Controller.hs
@@ -1,95 +1,98 @@
-module Controller where
-import ParserJson
 import Tipos
 import Data.List (groupBy)
+import ParserJson
 
+--- Auxiliares
+filtro []  _ = []
+filtro (x:xs) ys = if elem x ys then (filtro xs ys) ++ [x] else (filtro xs ys)
 -- Checar Ano
 checarPorAno a transations = a == ((year . datas) transations)
-
--- Filtrar transações por ano.
-filterPorAno year = do
-    transations <- getTransations
-    return (filter (checarPorAno year) transations)
-
 -- Checar Mes
-checarPorMes m  transations = m == ((month . datas) transations)
+checarPorMes m transations = m == ((month . datas) transations)
+-- Checando o dia
+checarDia d t = d == ((dayOfMonth . datas) t)
+-- Filtrar transações por ano.
+filterAno year = do
+    transactions <- getTransations
+    return (((filter . checarPorAno) year) transactions)
 
 -- Filtrar transações por ano e mês.
-filterPorAnoMes year month = do
-    filterPorAno <- filterPorAno year
-    return (filter (checarPorMes month) filterPorAno)
-
-
+filterAnoMes ano mes = do
+    anos <- filterAno ano
+    return (((filter . checarPorMes) mes) anos)
+    
 -- Verificando se a 'Transacao' é uma receita ou despesa
 -- não pode ser do tipo 'APLICACAO' nem 'VALOR_APLICACAO'
+isReceiptOrDebt :: Transacao -> Bool
+isReceiptOrDebt t = (filtro (tipos t) [APLICACAO, SALDO_CORRENTE, VALOR_APLICACAO]) == []
 
-isReceitaOuDespesa t = not (elem "APLICACAO" (tipos t)  || elem "VALOR_APLICACAO" (tipos t))
+getCreditosDebitos ano mes = do
+    transations <- (filterAnoMes ano mes)
+    return (filter isReceiptOrDebt transations)
 
--- Verificando se é uma receita
-isReceita t = (isReceitaOuDespesa t) && (valor t) >= 0
+-- Verificando se a 'Transacao' é uma receita
+ehReceita transations = (valor transations) > 0
 
--- Verificando se é uma despesa
-isDespesa t = (isReceitaOuDespesa t) && (valor t) < 0
+-- Verificando se a 'Transacao' é uma despesa
 
--- Removendo a transação SALDO_CORRENTE
+ehDespesa transations = (valor transations) < 0
 
--- Retorna receita
-getReceitas ano mes =  do
-    transations <- (filterPorAnoMes ano mes)
-    return (drop 1 (filter isReceita transations))
+-- Retorna a lista de débitos
+getDebitos ano mes =  do
+    transations <- (getCreditosDebitos ano mes)
+    return (filter ehDespesa transations)
 
--- Retorna despesas isExpense
-getDespesas ano mes =  do
-    transations <- (filterPorAnoMes ano mes)
-    return (drop 1 (filter isDespesa transations))
+-- Retorna a lista de créditos
+getCreditos ano mes =  do
+    transations <- (getCreditosDebitos ano mes)
+    return (filter ehReceita transations)
 
 -- Calcular o valor das receitas (créditos) em um determinado mês e ano.
-
 calculaCredito ano mes = do
-    transations <- getReceitas ano mes
-    return ((sum.(map valor)) transations)
+    transationsCretidos <- getCreditos ano mes
+    return ((sum.(map valor)) transationsCretidos)
 
+-- Calcular o valor das despesas (débitos) em um determinado mês e ano.
 calculaDebito ano mes = do
-    transations <- getDespesas ano mes
-    return ((sum.(map valor)) transations)
+    transationsDebitos <- getDebitos ano mes
+    return ((sum.(map valor)) transationsDebitos)
 
 -- Calcular a sobra (receitas - despesas) de determinado mês e ano
 calculaSobra ano mes = do
-    transations <- filterPorAnoMes ano mes
-    credito <- (calculaCredito ano mes)
-    debito <- (calculaDebito ano mes)
-    return (credito - debito)
+    credit <- (calculaCredito ano mes)
+    debit <- (calculaDebito ano mes)
+    return (credit + debit)
 
 -- Calcular o saldo final em um determinado ano e mês
 calculaSaldoFinalAnoMes ano mes = do
-    transations <- (filterPorAnoMes ano mes)
-    saldo <- (calculaSobra ano mes)
-    return (auxcalculaSaldoMes transations saldo)
-auxcalculaSaldoMes [] _ = 0
-auxcalculaSaldoMes transations saldo = saldo + (valor (transations !! 0))
+    baseBalance <- (initialBalance ano mes)
+    remainer <- (calculaSobra ano mes)
+    return (baseBalance + remainer)
 
 -- Calcular o saldo máximo atingido em determinado ano e mês
 getSaldoMax ano mes = do
-    transations <- filterPorAnoMes ano mes
-    return (auxSaldoMax transations)
+    balanco <- (initialBalance ano mes)
+    transations <- (getCreditosDebitos ano mes)
+    return (auxSaldoMax transations balanco)
 
+auxSaldoMax [] _= 0
+auxSaldoMax transactions monthBalance = ( maximum (balances (reverse (monthBalance:(map valor transactions)))))
+    
 -- Calcular o saldo mínimo atingido em determinado ano e mês
-getSaldoMin ano mes = do
-    transations <- filterPorAnoMes ano mes
-    return (auxSaldoMin transations)
+getSaldoMin ano mes= do
+    balanco <- (initialBalance ano mes)
+    transations <- (getCreditosDebitos ano mes)
+    return (auxSaldoMin transations balanco)
 
-listSaldo [x] = []
-listSaldo (x:y:xs) = [(valor x) + (valor y)] ++ (listSaldo (y:xs))
+auxSaldoMin [] _= 0
+auxSaldoMin transations monthBalance = ( minimum (balances (reverse (monthBalance:(map valor transations)))))
 
-auxSaldoMin [] = 0
-auxSaldoMin transations = ((valor (transations !! 0)) + ( minimum (listSaldo transations)))
-
-auxSaldoMax []  = 0
-auxSaldoMax transations = ((valor (transations !! 0)) + ( maximum (listSaldo transations)))
+--lista com os balanços
+balances [x] = [x]
+balances (x:xs) = (x + (sum xs):(balances xs))
 
 -- Calcular a média das receitas em determinado ano
 getMediaCreditoAno ano = (auxMediaCreditoAno ano)
-
 auxMediaCreditoAno ano = do
    total <- sequence (map(calculaCredito ano) [1..12])
    return ((sum total) / meses)
@@ -97,17 +100,14 @@ auxMediaCreditoAno ano = do
         meses = 12
 -- Calcular a média das despesas em determinado ano
 getMediaDebitoAno ano = (auxMediaDebitoAno ano)
-
 auxMediaDebitoAno ano = do
    total <- sequence (map(calculaDebito ano) [1..12])
    return ((sum total) / meses)
    where 
         meses = 12
 
-
 -- Calcular a média das sobras em determinado ano
 getMediaSobrasAno ano = (auxMediaSobrasAno ano)
-
 auxMediaSobrasAno ano = do
    total <- sequence (map(calculaSaldoFinalAnoMes ano) [1..12])
    return ((sum total) / meses)
@@ -115,14 +115,29 @@ auxMediaSobrasAno ano = do
         meses = 12
 
 
+-- Auxiliares FALTA AJEITAR 
+firstTransaction y m = do
+    transactions <- (filterAnoMes y m)
+    return (_firstTransaction transactions)
 
+_firstTransaction [] = []
+_firstTransaction xs = [(xs !! 0)]
+
+initialBalance :: Int -> Int -> IO Double
+initialBalance y m = do
+    transaction <- (firstTransaction y m)
+    return (_initialBalance transaction)
+
+_initialBalance :: [Transacao] -> Double
+_initialBalance [] = 0
+_initialBalance xs = valor (xs !! 0)
 
 -- Retornar o fluxo de caixa de determinado mês/ano. 
 -- O fluxo de caixa nada mais é do que uma lista contendo pares (dia,saldoFinalDoDia). 
 
 getFluxo year month = do
-    expenses <- (getDespesas year month)
-    incomes <- (getReceitas year month)
+    expenses <- (getDebitos year month)
+    incomes <- (getCreditos year month)
     return (_getCashFlow (ordenaDecrescente (expenses ++ incomes)))
 
 _getCashFlow transations = map sumDayFlow (groupBy diaIgual transations)
@@ -137,7 +152,7 @@ diaIgual tran1 tran2 = result
 
 -- Ordena uma lista de Transacao por dia
 ordenaDecrescente [] = []
-ordenaDecrescente (x:xs) = (ordenaDecrescente dir) ++ [x] ++ (ordenaDecrescente esq)
+ordenaDecrescente (x:xs) = (ordenaDecrescente esq) ++ [x] ++ (ordenaDecrescente dir)
     where 
         esq = filter (menor x) xs
         dir =  filter (maior x) xs
---
+++ Utils.hs
@@ -0,0 +1,67 @@
+module Utils (
+    intersect,
+    checkDay,
+    checkMonth,
+    checkYear,
+    filterByYear,
+    filterByYearAndMonth,
+    filterByYearMonthAndDay,
+    initialBalance,
+    firstTransaction
+) where
+
+import Tipos
+import ParserJson
+
+intersect []  _ = []
+intersect (x:xs) ys
+    | x `elem` ys = [x]++(intersect xs ys)
+    | otherwise = (intersect xs ys)
+
+-- Checando o ano 
+checkYear :: Int -> Transacao -> Bool
+checkYear y t = y == ((year . datas) t)
+
+-- Checando o mês
+checkMonth :: Int -> Transacao -> Bool
+checkMonth m  t = m == ((month . datas) t)
+
+-- Checando o dia
+checkDay :: Int -> Transacao -> Bool
+checkDay d t = d == ((dayOfMonth . datas) t)
+
+-- Filtrar transações por ano.
+filterByYear :: Int -> IO [Transacao]
+filterByYear year = do
+    transactions <- getTransactions
+    return (((filter . checkYear) year) transactions)
+    
+-- Filtrar transações por ano e mês.
+filterByYearAndMonth :: Int -> Int  -> IO [Transacao]
+filterByYearAndMonth year month = do
+    filteredByYear <- filterByYear year
+    return (((filter . checkMonth) month) filteredByYear)
+    
+    -- Filtrar transações por ano, mês e dia.
+filterByYearMonthAndDay :: Int -> Int -> Int -> IO [Transacao]
+filterByYearMonthAndDay year month day = do
+    filteredByMonth <- (filterByYearAndMonth year month)
+    return (((filter . checkDay) day) filteredByMonth)
+
+firstTransaction :: Int -> Int -> IO [Transacao]
+firstTransaction y m = do
+    transactions <- (filterByYearAndMonth y m)
+    return (_firstTransaction transactions)
+
+_firstTransaction :: [Transacao] -> [Transacao]
+_firstTransaction [] = []
+_firstTransaction xs = [(xs !! 0)]
+
+initialBalance :: Int -> Int -> IO Double
+initialBalance y m = do
+    transaction <- (firstTransaction y m)
+    return (_initialBalance transaction)
+
+_initialBalance :: [Transacao] -> Double
+_initialBalance [] = 0
+_initialBalance xs = valor (xs !! 0)
