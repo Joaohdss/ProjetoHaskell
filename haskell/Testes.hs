d9cfbf7fab5c7fb5e1719fad6a1f275141a0ff3c
caiosbl <caio.lira@ccc.ufcg.edu.br>
2019-11-27 13:40:42 -0300

    Adicionar Readme

--- haskell/Testes.hs
+++ haskell/Testes.hs
@@ -1,16 +1,12 @@
 module Testes
 where
-import Controller
+import Controller as Controller
 import Test.HUnit
 import ParserJson
 import Tipos
 import Data.List (groupBy)
 import Test.QuickCheck
 
-result = 92274.23
-calcula = calculaCredito 2017 3
-nome = "ovo"
-teste x = result == (4)
 
-main = do
-    quickCheck (teste :: [Int] -> Bool)
+
+
